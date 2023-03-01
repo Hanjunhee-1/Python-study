c4b058b4c705925e82448b7b25ac1c32abb06753
Hanjunhee-1 <hanjuni857@naver.com>
2023-03-02 03:09:33 +0900

    PlayGround.py 내용추가

--- PlayGround.py
+++ PlayGround.py
@@ -1,3 +1,21 @@
-a, b, c = map(int, input().split())
+# a, b, c = map(int, input().split())
 
-print(a + b + c)
+# print(a + b + c)
+
+
+
+# result_str = 'int'
+# n = int(input())
+
+# while(n > 0):
+#     result_str = 'long ' + result_str
+#     n -= 4
+
+# print(result_str)
+
+
+
+# t = int(input())
+# for i in range(t):
+#     n = input()
+#     print(n[0:1] + n[len(n)-1:len(n)])
