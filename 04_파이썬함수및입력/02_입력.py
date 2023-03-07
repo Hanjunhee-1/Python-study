9e3a8ea8ca4b35bbdc9dc2565b95c89b54628755
Hanjunhee-1 <hanjuni857@naver.com>
2023-03-08 02:39:11 +0900

    02_입력.py 작성

---
+++ 04_파이썬함수및입력/02_입력.py
@@ -0,0 +1,12 @@
+# input() 사용법
+# 기본 타입은 str
+
+# name = input('Enter your name: ')
+# grade = input('Enter your grade: ')
+
+# print(name, grade)
+# print(type(name), type(grade))
+
+
+# 한번에 입력받기
+print('{}'.format(int(input('원하는 숫자를 입력해주세요: '))))  # 이런건 처음봐서 좀 신기했다
