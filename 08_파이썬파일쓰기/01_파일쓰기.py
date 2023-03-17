72c1bd96de593a7aa51dd913b4bf5b5f9b5614b7
Hanjunhee-1 <hanjuni857@naver.com>
2023-03-18 00:50:11 +0900

    01_파일쓰기.py 내용추가

--- 08_파이썬파일쓰기/01_파일쓰기.py
+++ 08_파이썬파일쓰기/01_파일쓰기.py
@@ -29,3 +29,51 @@ print()
 
 # 다 사용한뒤에는 반드시 close()
 f.close()
+
+
+
+# 예제2
+# 예제1 에 나왔던 것처럼 해도 되지만 다음과 같은 방법도 있다.
+# 굳이 close 를 따로 해주지 않아도 되고 들여쓰기 덕분에 가독성이 좋다.
+# with open('./readTest/test1.txt', 'rt', encoding='UTF-8') as f:
+#     c = f.read()
+#     print(c)
+#     print(iter(c))
+#     print(list(c))
+# print()
+
+
+# 예제3
+# read(): 전체 읽기
+# read(10): 10byte 씩 읽기
+
+with open('./readTest/test2.txt', 'r', encoding='UTF-8') as f:
+    c = f.read(10)  # 10byte 갖고 오고 끝난 지점에 cursor 를 남김
+    print(c)
+    c = f.read(10)  # cursor 가 남았던 지점 이후에 10byte 를 가져옴.
+    print(c)
+    f.seek(0,0)     # 모든 cursor 초기화. 처음 부분으로 돌아감.
+    c = f.read(10)
+    print(c)
+print()
+
+
+# 예제4
+# readline: 한 줄씩 읽기
+with open('./readTest/test1.txt', 'r', encoding='UTF-8') as f:
+    line = f.readline()
+    print(line)
+    line = f.readline()
+    print(line)
+print()
+
+
+# 예제5
+# readlines: 전체를 읽은 후에 라인 단위로 리스트 형태 저장
+with open('./readTest/test1.txt', 'r', encoding='UTF-8') as f:
+    content = f.readlines()
+    print(content)
+    print()
+    for c in content:
+        print(c, end='')
+print()
